30eb3da7afadd8eb7d302acdee7287d7b23665d5
Ivan Morykit <ivanmorikit@gmail.com>
2026-03-04 16:48:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import re
+import re, os
 from flask import Flask, jsonify, request
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.proxies import GenericProxyConfig
 
 app = Flask(__name__)
+
+PROXY_HOST = os.environ.get('PROXY_HOST')
+PROXY_PORT = os.environ.get('PROXY_PORT')
+PROXY_USER = os.environ.get('PROXY_USER')
+PROXY_PASS = os.environ.get('PROXY_PASS')
 
 def clean_text(text):
     text = re.sub(r'[\u266a\u266b\u2669\u266c]', '', text)
@@ -18,7 +24,12 @@ def transcript():
     if not video_id:
         return jsonify({'error': 'Потрібен параметр id'}), 400
     try:
-        ytt = YouTubeTranscriptApi()
+        proxy_url = f"http://{PROXY_USER}:{PROXY_PASS}@{PROXY_HOST}:{PROXY_PORT}"
+        proxy_config = GenericProxyConfig(
+            http_url=proxy_url,
+            https_url=proxy_url
+        )
+        ytt = YouTubeTranscriptApi(proxies=proxy_config)
         data = ytt.fetch(video_id, languages=['uk', 'ru', 'en'])
         text = clean_text(' '.join([s.text for s in data]))
         return jsonify({'transcript': text, 'video_id': video_id})
